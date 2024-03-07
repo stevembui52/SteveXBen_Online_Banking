73f6a704df09cf9428ea69bf70a6b6ee20cb482e
stevembui52 <stevembui52@gmail.com>
2024-03-07 12:22:48 +0300

    pushed

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from .customer_api import customer
 
-def create_app(app_config = True):
+def create_app(app_config=None):
 
     app = Flask(__name__)
 
-
+    @app.route("/")
+    def index():
+        return {"message": "hello children"}
+    
     app.register_blueprint(customer)
 
     return app
